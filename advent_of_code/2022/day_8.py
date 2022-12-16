a5012433f1168d41d29c033e058089fbc274b846
William Kraus <88288206+KrLiam@users.noreply.github.com>
2022-12-15 22:49:55 -0300

    add day 2022.8

---
+++ advent_of_code/2022/day_8.py
@@ -0,0 +1,54 @@
+from os import linesep
+
+
+def parse(input: str):
+    lines = input.split(linesep)
+
+    return [[*map(int, line)] for line in lines]
+
+
+def is_visible(trees: list[list[int]], i: int, j: int):
+    max_i = len(trees) - 1
+    max_j = len(trees[0]) - 1
+
+    if i == 0 or i == max_i or j == 0 or j == max_j:
+        return True
+
+    this_height = trees[i][j]
+    row = trees[i]
+    column = [trees[n][j] for n in range(max_i + 1)]
+
+    for side in (row[:j], row[j + 1 :], column[:i], column[i + 1 :]):
+        if all(h < this_height for h in side):
+            return True
+
+    return False
+
+
+def get_view_distance(trees: list[list[int]], i: int, j: int):
+    this_height = trees[i][j]
+    row = trees[i]
+    column = [trees[n][j] for n in range(len(trees))]
+
+    score = 1
+
+    for side in (row[j - 1 :: -1], row[j + 1 :], column[i - 1 :: -1], column[i + 1 :]):
+        n = 0
+
+        for h in side:
+            n += 1
+
+            if h >= this_height:
+                break
+
+        score *= n
+
+    return score
+
+
+def part_2(trees: list[list[int]]):
+    return max(
+        get_view_distance(trees, i, j)
+        for i in range(len(trees))
+        for j in range(len(trees[0]))
+    )
