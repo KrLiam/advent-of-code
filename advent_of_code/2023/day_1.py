40e82bf002f00c4dccd30d4601976b3bc3a37a29
KrLiam <88288206+KrLiam@users.noreply.github.com>
2023-12-01 18:26:38 -0300

    add day 2023.1

---
+++ advent_of_code/2023/day_1.py
@@ -0,0 +1,41 @@
+import re
+
+
+def parse(input: str) -> list[str]:
+    return input.split("\n")
+
+
+def part_1(document: list[str]):
+    sum = 0
+
+    for line in document:
+        digits = re.findall(r"\d", line)
+        sum += int(digits[0] + digits[-1])
+
+    return sum
+
+
+DIGITS = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+
+def part_2(document: list[str]):
+    pattern = rf"(?=(\d|{'|'.join(DIGITS.keys())}))"
+
+    sum = 0
+    for line in document:
+        digits = re.findall(pattern, line)
+        first, last = digits[0], digits[-1]
+        sum += int(DIGITS.get(first, first) + DIGITS.get(last, last))
+    
+    return sum
+
